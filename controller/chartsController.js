e767ae23edf6663b0fa8a055bdcc9dc2e7b7e618
pradeepGreen00 <dev2@greensturn.com>
2024-06-19 18:41:17 +0530

    concrete and water

--- controller/chartsController.js
+++ controller/chartsController.js
@@ -1,3 +1,5 @@
+//
+const { ObjectId } = require('mongodb'); // Import ObjectId from MongoDB driver
 const energyProvider = require("../model/utilityEnergyProviderModel");
 const nonRenewable = require("../model/nonRenewableModel");
 const renwableModel = require("../model/renewableModel");
@@ -180,9 +182,8 @@ const energyPieConsumption = async (req, res) => {
 const energyLineConsumption = async (req, res) => {
   let { dateRange, projectId, interval, unit } = req.body;
   try {
-    const startDate = new Date(dateRange.start);
-    const endDate = new Date(dateRange.end);
-    let matchQuery = { projectId: projectId, createdAt: { $gte: startDate, $lte: endDate } };
+
+    let matchQuery = { projectId: projectId,packageId:packageId};
     let groupId;
 
     switch (interval) {
@@ -218,7 +219,7 @@ const energyLineConsumption = async (req, res) => {
     };
 
     const conversionPipeline = (consumptionField) => [
-      { $match: matchQuery },
+     { $match: matchQuery },
       {
         $project: {
           createdAt: 1,
@@ -244,6 +245,12 @@ const energyLineConsumption = async (req, res) => {
         $group: {
           _id: groupId,
           totalConsumption: { $sum: "$consumptionInDesiredUnit" }
+        }
+      },{
+        $project: {
+          _id: 0, // Hide the default _id field
+          result: "$_id", // Rename _id to newIdName
+          totalConsumption: 1 // Include the totalConsumption field
         }
       }
     ];
@@ -272,9 +279,355 @@ const energyLineConsumption = async (req, res) => {
 };
 
 
+const waterPieConsumption = async (req, res) => {
+  // Uncomment and use if you need to filter by date or month
+   let { dateRange,projectId } = req.body;
+  try {
+    const timestampsDate = new Date(dateRange);
+    const month = timestampsDate.getMonth()+1;
+    const year = timestampsDate.getFullYear();
+    console.log(month,year)
+    let query = {projectId:projectId}; // Define your query if needed, currently empty to match all documents
+    if(month&&year){
+      query = {
+        $expr: {
+          $and: [
+            { $eq: [{ $month: "$createdAt" }, month] },
+            { $eq: [{ $year: "$createdAt" }, year] }
+          ]
+        }
+      };
+    }if(year){
+      query = {
+        $expr: {
+          $and: [
+            { $eq: [{ $year: "$createdAt" }, year] }
+          ]
+        }
+      };
+    }
+    if(month){
+      query = {
+        $expr: {
+          $and: [
+            { $eq: [{ $month: "$createdAt" }, month] }
+          ]
+        }
+      };
+    }
+    /* total of thrice tables data
+    // Define the aggregation pipeline
+    const pipeline = [
+      {
+        $match: query
+      },
+      {
+        $project: {
+          consumption: 1 // Project `consumption` as is in `energyProvider`
+        }
+      },
+      {
+        $unionWith: {
+          coll: "EnergyRenewable",
+          pipeline: [
+            {
+              $match: query
+            },
+            {
+              $project: {
+                consumption: 1 // Project `consumption` as is in `EnergyRenewable`
+              }
+            }
+          ]
+        }
+      },
+      {
+        $unionWith: {
+          coll: "EnergyNonRenewable",
+          pipeline: [
+            {
+              $match: query
+            },
+            {
+              $project: {
+                consumption: "$energyOutput" // Project `energyOutput` as `consumption` in `EnergyNonRenewable`
+              }
+            }
+          ]
+        }
+      },
+      {
+        $group: {
+          _id: null,
+          totalCount: { $sum: "$consumption" }
+        }
+      }
+    ];
+
+    // Run the aggregation
+    const result = await energyProvider.aggregate(pipeline);
+*/
+    //
+    const conversionPipeline = (consumptionField) => [
+      { $match: query },
+      {
+        $project: {
+          consumptionInJoules: {
+            $switch: {
+              // branches: [
+              //   { case: { $eq: ["$unit", "Kwh"] }, then: { $multiply: [`$${consumptionField}`, 3600000] } },
+              //   { case: { $eq: ["$unit", "Wh"] }, then: { $multiply: [`$${consumptionField}`, 3600] } },
+              //   { case: { $eq: ["$unit", "Joule"] }, then: `$${consumptionField}` },
+              //   { case: { $eq: ["$unit", null] }, then: { $multiply: [`$${consumptionField}`, 3600000] } } // Default to kWh if unit is missing
+
+              // ],
+              //In KWH
+              branches: [
+                { case: { $eq: ["$unit", "Kwh"] }, then: `$${consumptionField}` },
+                { case: { $eq: ["$unit", "Wh"] }, then: { $divide: [`$${consumptionField}`, 1000] } },
+                { case: { $eq: ["$unit", "Joule"] }, then: { $divide: [`$${consumptionField}`, 3600000] } },
+                { case: { $eq: ["$unit", null] }, then: `$${consumptionField}` } // Default to kWh if unit is missing
+              ],
+              default: 0
+            }
+          }
+        }
+      },
+      {
+        $group: {
+          _id: null,
+          totalConsumption: { $sum: "$consumptionInJoules" }
+        }
+      }
+    ];
+
+    const [bottleConsumption, waterProviderConsumption] = await Promise.all([
+      bottleWaterModel.aggregate(conversionPipeline('consumption')),
+      waterProviderModel.aggregate(conversionPipeline('energyOutput'))
+    ]);
+
+    const totalBottleConsumption = bottleConsumption.length > 0 ? bottleConsumption[0].totalConsumption : 0;
+    const totalWaterProviderConsumption = waterProviderConsumption.length > 0 ? waterProviderConsumption[0].totalConsumption : 0;
+
+    console.log(totalWaterProviderConsumption, "totalNonRenewableConsumption", totalBottleConsumption, "totalProviderEnergyConsumption");
+    const totalWaterConsumption = totalBottleConsumption + totalWaterProviderConsumption
+
+    // Log and return the result
+    // console.log(result, "result");
+    const bottlePercentage = totalBottleConsumption > 0 ? (totalBottleConsumption / totalWaterConsumption) * 100 : 0;
+    const waterPercentage = totalWaterProviderConsumption > 0 ? (totalWaterProviderConsumption / totalWaterConsumption) * 100 : 0;
+
+    console.log(`Renewable Consumption: ${bottlePercentage.toFixed(2)}%`);
+    console.log(`Non-Renewable Consumption: ${waterPercentage.toFixed(2)}%`);
+    
+    return res.status(200).send({
+      status: true,
+      message: "Energy Consumption",
+      bottlePercentage:bottlePercentage,
+      waterPercentage:waterPercentage,
+      totalBottleConsumption:totalBottleConsumption,
+      totalWaterConsumption:totalWaterConsumption,
+
+
+      // result: result
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send({ error: error.message,status:false });
+  }
+};
+const waterLineConsumption = async (req, res) => {
+  let { dateRange, projectId, interval, unit } = req.body;
+  try {
+
+    let matchQuery = { projectId: projectId,packageId:packageId};
+    let groupId;
+
+    switch (interval) {
+      case 'monthly':
+        groupId = { year: { $year: "$createdAt" }, month: { $month: "$createdAt" } };
+        break;
+      case 'quarterly':
+        groupId = {
+          year: { $year: "$createdAt" },
+          quarter: {
+            $ceil: { $divide: [{ $month: "$createdAt" }, 3] }
+          }
+        };
+        break;
+      case 'yearly':
+        groupId = { year: { $year: "$createdAt" } };
+        break;
+      default:
+        return res.status(400).send({ status: false, message: "Invalid interval specified" });
+    }
+
+    const unitConversionFactor = (unit) => {
+      switch (unit) {
+        case 'Kwh':
+          return 1; // kWh is the base unit
+        case 'Wh':
+          return 1000; // 1 kWh = 1000 Wh
+        case 'Joule':
+          return 3600000; // 1 kWh = 3600000 Joules
+        default:
+          return 1; // Default to kWh if unit is not specified
+      }
+    };
+
+    const conversionPipeline = (consumptionField) => [
+     { $match: matchQuery },
+      {
+        $project: {
+          createdAt: 1,
+          consumptionInDesiredUnit: {
+            $divide: [
+              {
+                $switch: {
+                  branches: [
+                    { case: { $eq: ["$unit", "Kwh"] }, then: `$${consumptionField}` },
+                    { case: { $eq: ["$unit", "Wh"] }, then: { $divide: [`$${consumptionField}`, 1000] } },
+                    { case: { $eq: ["$unit", "Joule"] }, then: { $divide: [`$${consumptionField}`, 3600000] } },
+                    { case: { $eq: ["$unit", null] }, then: `$${consumptionField}` } // Default to kWh if unit is missing
+                  ],
+                  default: 0
+                }
+              },
+              unitConversionFactor(unit)
+            ]
+          }
+        }
+      },
+      {
+        $group: {
+          _id: groupId,
+          totalConsumption: { $sum: "$consumptionInDesiredUnit" }
+        }
+      },{
+        $project: {
+          _id: 0, // Hide the default _id field
+          result: "$_id", // Rename _id to newIdName
+          totalConsumption: 1 // Include the totalConsumption field
+        }
+      }
+    ];
+
+    const [renewableConsumption, nonRenewableConsumption, providerEnergyConsumption] = await Promise.all([
+      renwableModel.aggregate(conversionPipeline('consumption')),
+      nonRenewable.aggregate(conversionPipeline('energyOutput')),
+      energyProvider.aggregate(conversionPipeline('consumption'))
+    ]);
+
+    const totalRenewableConsumption = renewableConsumption.length > 0 ? renewableConsumption : [];
+    const totalNonRenewableConsumption = nonRenewableConsumption.length > 0 ? nonRenewableConsumption : [];
+    const totalProviderEnergyConsumption = providerEnergyConsumption.length > 0 ? providerEnergyConsumption : [];
+
+    return res.status(200).send({
+      status: true,
+      message: "Energy Consumption",
+      totalNonRenewableConsumption,
+      totalProviderEnergyConsumption,
+      totalRenewableConsumption
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send({ error: error.message, status: false });
+  }
+};
+
+
+
+
+
+const concretePieConsumption = async (req, res) => {
+  let { dateRange, projectId ,packageId} = req.body;
+  try {
+    const timestampsDate = new Date(dateRange);
+    const month = timestampsDate.getMonth() + 1;
+    const year = timestampsDate.getFullYear();
+    console.log(month, year);
+    let projectIdd = new ObjectId(projectId);
+    let packageIdd = new ObjectId(packageId);
+
+
+    let query = { projectId: projectIdd ,packageId:packageIdd }; // Base query
+
+    // Adjust the query based on the date range
+    if (month && year) {
+      query.$expr = {
+        $and: [
+          { $eq: [{ $month: "$createdAt" }, month] },
+          { $eq: [{ $year: "$createdAt" }, year] }
+        ]
+      };
+    } else if (year) {
+      query.$expr = {
+        $eq: [{ $year: "$createdAt" }, year]
+      };
+    } else if (month) {
+      query.$expr = {
+        $eq: [{ $month: "$createdAt" }, month]
+      };
+    }
+
+    // Define the aggregation pipeline
+    const pipeline = [
+      {
+        $match: query
+      },
+      {
+        $group: {
+          _id: "$type",
+          totalVolume: { $sum: "$volume" }
+        }
+      },
+      {
+        $group: {
+          _id: null,
+          totalVolumes: { $sum: "$totalVolume" },
+          details: { $push: { type: "$_id", totalVolume: "$totalVolume" } }
+        }
+      },
+      {
+        $unwind: "$details"
+      },
+      {
+        $project: {
+          type: "$details.type",
+          totalVolume: "$details.totalVolume",
+          percentage: {
+            $multiply: [
+              { $divide: ["$details.totalVolume", "$totalVolumes"] },
+              100
+            ]
+          }
+        }
+      }
+    ];
+
+    // Run the aggregation
+    const result = await concreteMixModel.aggregate(pipeline);
+
+    return res.status(200).send({
+      status: true,
+      message: "Concrete Consumption",
+      result: result
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send({ error: error.message, status: false });
+  }
+};
+
+
+
+
 // energyConsumption();
 
 module.exports ={
   energyPieConsumption,
-  energyLineConsumption
+  energyLineConsumption,
+  waterPieConsumption,
+  waterLineConsumption,
+  concretePieConsumption
 }
--- routes/v1/charts.js
+++ routes/v1/charts.js
@@ -3,6 +3,10 @@ const auth = require('../../middleware/authHandler');
 const chartsInstance = require('../../controller/chartsController');
 
 router.post("/energy/pie",chartsInstance.energyPieConsumption);
-router.post("/energy/line",chartsInstance.energyPieConsumption);
+router.post("/energy/line",chartsInstance.energyLineConsumption);
+
+router.post("/water/pie",chartsInstance.waterPieConsumption);
+
+router.post("/concrete/pie",chartsInstance.concretePieConsumption);
 
 module.exports = router;
